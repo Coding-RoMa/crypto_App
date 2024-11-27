210568a2f069ee597c21d647c43dabeca530eaf4
Rosalia Mazza <68191204+Coding-RoMa@users.noreply.github.com>
2024-11-27 19:37:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ if not df.empty and 'Adj Close' in df.columns:
 
     # Rename the column at position 5 (index 4)
     #df.rename(columns={df.columns[6]: "Bollinger Bands"}, inplace=True)
-
+"""
     # Add MultiIndex for column grouping
     columns = [
         ("Price Data", "High"),
@@ -70,7 +70,7 @@ if not df.empty and 'Adj Close' in df.columns:
     ]
     df.columns = pd.MultiIndex.from_tuples(columns)
 
-
+"""
 
 
 
